05e226ef9bbf8d8c8e4749002589c277351f5619
sebastianhincapiesalazar-source <sebastian.hincapie.salazar@est.una.ac.cr>
2026-09-06 20:07:00 -0600

    Update BotAgresivo.java

--- BotAgresivo.java
+++ BotAgresivo.java
@@ -1,38 +1,16 @@
-package com.una.pokerdefichas.juego;
+package com.una.pokerdefichas.modelo;
 
-import com.una.pokerdefichas.modelo.AccionPoker;
-import com.una.pokerdefichas.modelo.Jugador;
 
-/*
- * Estrategia agresiva para un jugador bot.
- *
- * Este bot intenta presionar a los demás jugadores
- * mediante aumentos de las apuestas.
- */
+ /** El bot intenta aumentar la apuesta cuando puede.*/
 public class BotAgresivo implements EstrategiaApuesta {
 
-    /*
-     * Determina qué acción realizará el bot.
-     */
     @Override
-    public AccionPoker decidirAccion(
-            Jugador jugador,
-            int apuestaActual,
-            int boteActual
-    ) {
+    public AccionPoker decidirAccion(int apuestaActual, int boteActual) {
 
-        /*
-         * Si todavía no existe una apuesta,
-         * el bot realiza un RAISE para comenzar agresivamente.
-         */
         if (apuestaActual == 0) {
             return AccionPoker.RAISE;
         }
 
-        /*
-         * Si ya existe una apuesta,
-         * el bot intenta aumentarla nuevamente.
-         */
-        return AccionPoker.RAISE;
+        return AccionPoker.CALL;
     }
 }
