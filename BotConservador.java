5af4b43aaa4d7334c65509e499fd38fcdda65059
sebastianhincapiesalazar-source <sebastian.hincapie.salazar@est.una.ac.cr>
2026-09-06 20:08:27 -0600

    Update BotConservador.java

--- BotConservador.java
+++ BotConservador.java
@@ -1,47 +1,24 @@
-package com.una.pokerdefichas.juego;
-
-import com.una.pokerdefichas.modelo.AccionPoker;
-import com.una.pokerdefichas.modelo.Jugador;
+package com.una.pokerdefichas.modelo;
 
 /*
- * Estrategia conservadora para un jugador bot.
- *
- * Este bot intenta reducir los riesgos durante
- * las rondas de apuestas.
+ * El bot intenta evitar riesgos:
+ * - Si no hay apuesta, hace CHECK.
+ * - Si hay una apuesta pequeña, hace CALL.
+ * - Si la apuesta es demasiado alta, hace FOLD.
  */
 public class BotConservador implements EstrategiaApuesta {
 
-    /*
-     * Determina qué acción realizará el bot
-     * dependiendo de la apuesta actual.
-     */
     @Override
-    public AccionPoker decidirAccion(
-            Jugador jugador,
-            int apuestaActual,
-            int boteActual
-    ) {
+    public AccionPoker decidirAccion(int apuestaActual, int boteActual) {
 
-        /*
-         * Si nadie ha realizado una apuesta,
-         * el bot utiliza CHECK para no arriesgar fichas.
-         */
         if (apuestaActual == 0) {
             return AccionPoker.CHECK;
         }
 
-        /*
-         * Si la apuesta es relativamente pequeña,
-         * el bot decide igualarla.
-         */
-        if (apuestaActual <= 100) {
+        if (apuestaActual <= 20) {
             return AccionPoker.CALL;
         }
 
-        /*
-         * Si la apuesta es demasiado alta,
-         * el bot conservador se retira.
-         */
         return AccionPoker.FOLD;
     }
 }
