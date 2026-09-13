d065d5cbba9a91903e3aa037b44e973c509b70a0
Cristian Coronado <criscoroch27@gmail.com>
2026-09-13 12:17:14 -0600

    Update Palo.java

--- PokerDeFichas/src/model/Palo.java
+++ PokerDeFichas/src/model/Palo.java
@@ -1,7 +1,11 @@
+
 package model;
 /*
     Representa los palos posibles de la baraja
     Cada valor incluye su símbolo asociado para facilitar la representación visual en la GUI
+ */
+/**
+ * Enumera los cuatro palos de una baraja y su símbolo visual.
  */
 public enum Palo {
     //se utiliza enum cuando se conocen todos los valores posibles de una variable en tiempo de compilación
@@ -17,5 +21,9 @@ public enum Palo {
         this.simbolo = simbolo;
     }
     //getSimbolo() retorna el simbolo correspondiente
+/**
+ * Obtiene el símbolo del palo.
+ * @return resultado de tipo String.
+ */
     public String getSimbolo() {return simbolo; }
 }
