229e48c1f05f7dc503b09654d05e25d04638621b
LeriaaU0207 <valeria.bahena.mujica@est.una.ac.cr>
2026-09-12 23:15:03 -0600

    Se eliminan las copias de archivos que se generaron

--- AccionPoker.java
+++
@@ -1,8 +0,0 @@
-package com.una.pokerdefichas.modelo;
-
-//Posibles acciones que un jugador puede tomar durante su turno
-
-public enum AccionPoker {
-    //enum al conocer las posibles acciones durante el juego
-    CHECK, CALL, FOLD, RAISE
-}
--- EstrategiaApuesta.java
+++
@@ -1,8 +0,0 @@
-package com.una.pokerdefichas.modelo;
-
-
-public interface EstrategiaApuesta {
-
-    
-    AccionPoker decidirAccion(int apuestaActual, int boteActual);
-}
