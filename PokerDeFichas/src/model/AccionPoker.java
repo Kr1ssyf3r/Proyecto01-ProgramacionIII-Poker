43d1513294e173d81512999ecc0abb1bdd70cedf
Cristian Coronado <criscoroch27@gmail.com>
2026-09-13 12:20:14 -0600

    Document AccionPoker enum actions
    Added documentation for AccionPoker enum.

--- PokerDeFichas/src/model/AccionPoker.java
+++ PokerDeFichas/src/model/AccionPoker.java
@@ -2,6 +2,9 @@ package model;
 
 //Posibles acciones que un jugador puede tomar durante su turno
 
+/**
+ * Enumera las acciones que un jugador puede realizar durante una ronda de póker.
+ */
 public enum AccionPoker {
     //enum al conocer las posibles acciones durante el juego
     CHECK, CALL, FOLD, RAISE
