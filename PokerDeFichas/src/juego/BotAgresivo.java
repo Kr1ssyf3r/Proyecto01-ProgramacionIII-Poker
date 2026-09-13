9560326783e12cf02c67d1a1193183d701f51528
Cristian Coronado <criscoroch27@gmail.com>
2026-09-13 12:10:07 -0600

    Add Javadoc for decidirAccion method in BotAgresivo
    Added Javadoc comments to the decidirAccion method.

--- PokerDeFichas/src/juego/BotAgresivo.java
+++ PokerDeFichas/src/juego/BotAgresivo.java
@@ -6,6 +6,12 @@ import model.AccionPoker;
 /** El bot intenta aumentar la apuesta cuando puede.*/
 public class BotAgresivo implements EstrategiaApuesta {
 
+/**
+ * Determina la acción que debe realizar el jugador o bot según la apuesta y el bote actuales.
+ * @param apuestaActual valor utilizado por el método para realizar su operación.
+ * @param boteActual valor utilizado por el método para realizar su operación.
+ * @return valor calculado o recuperado por el método.
+ */
     @Override
     public AccionPoker decidirAccion(int apuestaActual, int boteActual) {
 
