f207d5d694e744ccd397a054e278edf9add78329
Cristian Coronado <criscoroch27@gmail.com>
2026-09-13 12:14:24 -0600

    Add JavaDoc comments to Main class
    Added JavaDoc comments for the main class and method.

--- PokerDeFichas/src/view/Main.java
+++ PokerDeFichas/src/view/Main.java
@@ -2,7 +2,14 @@ package view;
 
 import javafx.application.Application;
 
+/**
+ * Punto de entrada de la aplicación JavaFX.
+ */
 public class Main {
+/**
+ * Inicia la aplicación JavaFX utilizando la ventana principal.
+ * @param args valor utilizado por el método para realizar su operación.
+ */
     public static void main(String[] args) {
         Application.launch(VentanaPrincipal.class, args);
     }
